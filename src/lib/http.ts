ed1cd4fe66865030118df52387e372bb9f251745
Jose Galvis <41926029+johu19@users.noreply.github.com>
2026-06-16 15:15:10 -0500

    fix: cors origin (#8)

--- src/lib/http.ts
+++ src/lib/http.ts
@@ -1,5 +1,11 @@
 import { APIGatewayProxyStructuredResultV2 } from 'aws-lambda';
 
+const corsHeaders = {
+  'access-control-allow-origin': '*',
+  'access-control-allow-headers': 'authorization,content-type',
+  'access-control-allow-methods': 'GET,POST,PATCH,DELETE,OPTIONS',
+};
+
 export function json(
   statusCode: number,
   payload: unknown,
@@ -7,6 +13,7 @@ export function json(
   return {
     statusCode,
     headers: {
+      ...corsHeaders,
       'content-type': 'application/json',
     },
     body: JSON.stringify(payload),
